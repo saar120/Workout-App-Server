4213eb65d58945c805a013fd8efdba06bedbe6c6
Saar Amrani <saar120@gmail.com>
2022-02-12 15:28:24 +0200

    working protoype

--- Controllers/workout.js
+++ Controllers/workout.js
@@ -1,3 +1,4 @@
+const Exercises = require("../Database/Models/exercisesModel.js");
 const UserWorkout = require("../Database/Models/userWorkouts.js");
 const { createWorkout, getWorkoutByID, getExercisesByNameAndID } = require("../utils/workoutUtils.js");
 
@@ -115,15 +116,9 @@ const suggestedMuscles = async (req, res) => {
       { $limit: 3 },
       {
         $group: {
-          _id: "$workouts.date",
-          exercises: { $first: "$workouts.exercises.name" },
-        },
-      },
-      {
-        $group: {
           _id: null,
           result: {
-            $addToSet: "$exercises",
+            $push: "$workouts.exercises.name",
           },
         },
       },
@@ -131,16 +126,61 @@ const suggestedMuscles = async (req, res) => {
         $project: {
           _id: 0,
           result: {
-            $reduce: {
-              input: "$result",
-              initialValue: [],
-              in: { $concatArrays: ["$$value", "$$this"] },
+            $setUnion: {
+              $reduce: {
+                input: "$result",
+                initialValue: [],
+                in: { $concatArrays: ["$$value", "$$this"] },
+              },
             },
           },
         },
       },
     ]);
-    console.log(userExercisesNames);
+    if (userExercisesNames.length === 0) {
+      const error = new Error("Exercises not found");
+      error.code = 404;
+      throw error;
+    }
+    const userLastExercises = userExercisesNames[0].result;
+    const allMuscles = await Exercises.aggregate([
+      { $group: { _id: null, result: { $push: "$primaryMuscles" } } },
+      {
+        $project: {
+          _id: 0,
+          result: {
+            $setUnion: {
+              $reduce: { input: "$result", initialValue: [], in: { $concatArrays: ["$$value", "$$this"] } },
+            },
+          },
+        },
+      },
+    ]);
+    const suggestedMuscles = await Exercises.aggregate([
+      { $match: { name: { $in: userLastExercises } } },
+      { $group: { _id: null, result: { $push: "$primaryMuscles" } } },
+      {
+        $project: {
+          _id: 0,
+          result: {
+            $setDifference: [
+              allMuscles[0].result,
+              {
+                $setUnion: {
+                  $reduce: { input: "$result", initialValue: [], in: { $concatArrays: ["$$value", "$$this"] } },
+                },
+              },
+            ],
+          },
+        },
+      },
+    ]);
+    if (suggestedMuscles.length === 0) {
+      const error = new Error("Muscles not found");
+      error.code = 500;
+      throw error;
+    }
+    console.log(suggestedMuscles[0].result);
   } catch (error) {
     if (error.code) {
       // return res.status(error.code).json({ message: error.message });
